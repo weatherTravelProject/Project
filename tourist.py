528363851c971066d072d63d6c39e7f8607eb363
husky9811 <1831488354@qq.com>
2020-10-29 23:09:59 +0800

    新增getPageNum，获取当前城市旅游景点页数进行爬取（不用写死）

--- tourist.py
+++ tourist.py
@@ -32,14 +32,34 @@ def getPage(url):  # 获取链接中的网页内容
     except Exception as e:
         print(str(e))
 
+def getPageNum(html):
+
+    num = int(html.xpath('//*[@id="pager-container"]/div/a[last()-1]/text()')[0])
+    return num
 
 def getList():
-    place = '大连'
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'cookie': 'QN1=O5cv7V3FXX6RVSo2FsZGAg==; QN205=s%3Dgoogle; QN277=s%3Dgoogle; csrfToken=AUBSZ1SJgtl2cBX2HyuS2BTEXvmciXKc; _i=RBTKS082R8KxQVZx6JB9OtPmNyxx; QN269=1EDA0650022211EA926DFA163E72396C; fid=3ac01f31-9181-4ed7-a38a-5c0d137aefcc; QN99=1500; QN300=organic; QunarGlobal=10.86.213.148_-72515c99_16e4b45ba14_623b|1573223249997; _vi=hCXRv3fD46caoElZPDCXIreqcskT0Spj7f9XqyWhjfxmL_u3Y7xhSmU49JoeKnflmT0XtDD78wrAbOYIR_IG3FwU-fkUzn927J70W0JK0Iy219BfJnqA_WGLcMw_p8UHFhEtG9kQ-SOa3-aG4lJkhxTME8JhUdpjG4Kl7_X1KFGy; QN601=28e00ebdfc5eae6e44c48a34869c6dd1; QN163=0; QN667=B; QN48=09fd9580-c998-4f2a-bd31-516ddfaeb4f3; QN100=WyLlpKfov57mma%2Fngrl85aSn6L%2BeIl0%3D; QN243=19; JSESSIONID=568D43DF4624B99A32042DCC50F984B8; QN57=15732237429980.552477010226762; Hm_lvt_15577700f8ecddb1a927813c81166ade=1573223744; QN267=91363111d04a42d3; QN58=1573223742995%7C1573223864461%7C2; Hm_lpvt_15577700f8ecddb1a927813c81166ade=1573223865; QN271=88a69a10-00a3-42e0-a9dd-dcd01d16ca04',
+        'Host': 'piao.qunar.com',
+        'Referer': 'http://piao.qunar.com/ticket/list.htm?keyword=%E7%83%AD%E9%97%'
+                   'A8%E6%99%AF%E7%82%B9&region=&from=mpl_search_suggest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+                      ' (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    place = '云浮'
     url = 'http://piao.qunar.com/ticket/list.htm?keyword=' + place + '&region=&from=mpl_search_suggest&page={}'
     # url = 'https://travel.qunar.com/p-cs300134-'+place+'-jingdian-1-{}'
+    htm = requests.get(url, headers=headers)
+    html = etree.HTML(htm.text)
+    num = getPageNum(html)
     i = 1
     sightlist = []
-    while i < 37:
+    while i < num:
         page = getPage(url.format(i))  # 这里调用了getPage函数获取了网页数据
         selector = etree.HTML(page.text)
         print('正在爬取第', str(i), '页景点信息')
@@ -99,7 +119,6 @@ def listToExcel(list, name):
 def main():
     sightlist = getList()  # main后第一个运行getList()
     listToExcel(sightlist, 'hotplace')
-
 
 if __name__ == '__main__':  # 代码是从main函数开始的
     main()
